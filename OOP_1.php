86706d7b44075345639f02236bb22a0a6ab7dfb5
sajjad0057 <sajjadhossain0057@gmail.com>
2021-06-27 01:28:42 +0600

    construct and static property in PHP class

--- OOP_1.php
+++ OOP_1.php
@@ -5,18 +5,43 @@ class Person{
     public $surname;
     public $age;
 
+    public static $counter = 0;
+
+    public function __construct($name,$surname){
+        echo $name . " " . $surname .PHP_EOL ;
+        $this->name = $name;
+        $this->surname = $surname;
+        self::$counter++;    // :: is called scope resulations. used to access static property in class .  
+    }
+
+    public function setAge($age){
+        $this ->age = $age;
+    }
+    public function getAge(){
+        return $this->age;
+    }
+
+
+    public static function getCounter(){
+        return self::$counter;   
+    }
+
+
+
 
 };
 
-$p1 = new Person();
-$p1->name = "sajjad hossain";
-$p1->surname = "sajjad";
-$p1->age = 25;
-
-$p2 = new Person();
-$p2->name = "sajjad hossain";
-$p2->surname = "sajjad";
-$p2->age = 25;
-
+$p1 = new Person("sajjad hossain","sajjad" );
+// $p1->name = "sajjad hossain";
+// $p1->surname = "sajjad";
+// $p1->age = 25;
 var_dump( $p1 );
-echo "<br>". $p1->name . "<br>";
+$p2 = new Person("sanwar hossain","sanwar");
+// $p2->name = "sanwar hossain";
+// $p2->surname = "sanwar";
+// $p2->age = 16;
+$p1->setAge(24);
+echo $p1->getAge() . PHP_EOL; 
+var_dump( $p1 );
+echo  $p2->name . PHP_EOL ;
+echo Person :: $counter . PHP_EOL;
